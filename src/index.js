61048092518513f9cb1c3881893d642044c82bfa
Umarjon <89200068+UZBase@users.noreply.github.com>
2021-10-05 17:28:29 +0500

    path add

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import path from "path";
 import dotenv  from 'dotenv'
 dotenv.config({ path: '/custom/path/to/.env' })
 ReactDOM.render(
