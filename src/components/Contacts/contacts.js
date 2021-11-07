edee78de51069e64e13aa6022f4b57349bb90a6b
UZBase <sharofiddinov.07@mail.ru>
2021-11-07 18:34:11 +0500

    123

--- src/components/Contacts/contacts.js
+++ src/components/Contacts/contacts.js
@@ -1,14 +1,62 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import "./contacts.css";
+import { ImTelegram, ImInstagram, ImYoutube, ImGithub } from "react-icons/im";
 
 function contacts() {
-    return (
-        <Container fluid className="contact-section">
-                    <Row>
-                        <h1>Вы ниже можете увидеть мои соц.сети</h1>
-                    </Row>
-        </Container>
-    );
+  return (
+    <Container fluid className="contact-section">
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          ></Col>
+          <h1 className="Big__text__contact">
+            Вы ниже можете увидеть мои соц.сети
+          </h1>
+          <Col md={7} >
+            <a
+              target="_blank"
+              href="https://t.me/Senior_Developper"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <ImTelegram className="telegram__icon" />
+              <h1 style={{ color: "#0088cc" }}>Telegram</h1>
+            </a>
+            <a
+              target="_blank"
+              href="https://www.instagram.com/um4rj0n/"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <ImInstagram className="instagram__icon" />
+              <h1 style={{ color: "#8a3ab9" }}>Instagram</h1>
+            </a>
+            <a
+              target="_blank"
+              href="https://www.youtube.com/c/UZBase"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <ImYoutube className="youtube__icon" />
+              <h1 style={{ color: "#ff0000" }}>Youtube</h1>
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/UZBase"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <ImGithub className="github__icon" />
+              <h1 style={{ color: "#171515" }}>Github</h1>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 }
 
 export default contacts;
